55cec437b0c042b7aab68a74274e89ebcfb969de
Nadi Adem <Nadem@spartaglobal.com>
2021-10-19 14:48:12 +0100

    changes made to settings

---
+++ WebLibrary/Migrations/20211017010346_SeededData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebLibrary.Migrations
+{
+    public partial class SeededData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "BookPlacement",
+                columns: new[] { "Id", "BookGenre", "ColumnNum" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 0, 2 },
+                    { 3, 2, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "Author", "BookGenre", "BookInfoId", "ImgUrl", "Publisher", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 12, "JJ Fish", 0, null, null, null, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 144, "A Mirra", 1, null, null, null, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 1, "Susan M", 2, null, null, null, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BookPlacement",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BookPlacement",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BookPlacement",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 144);
+        }
+    }
+}
